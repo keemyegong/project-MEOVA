f7a829f36ccf08fcefe915a10ac687b05b034966
cadetbluee <cadetbluee@icloud.com>
2024-05-18 19:03:56 +0900

    :memo:

--- front/meova/src/stores/review.js
+++ front/meova/src/stores/review.js
@@ -7,6 +7,47 @@ export const useReviewStore = defineStore("review", () => {
   const user = useUserStore();
   const router = useRouter();
   const BASE_URL = "http://127.0.0.1:8000/api/v1/movies";
-
-  return {};
+  const createReview = function (review) {
+    const { id, vote, title, content } = review;
+    axios({
+      method: "post",
+      url: `${BASE_URL}/${id}/reviews/`,
+      data: {
+        vote,
+        title,
+        content,
+      },
+      headers: {
+        Authorization: `Token ${user.token}`,
+      },
+    })
+      .then((res) => {
+        console.log("성공");
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const createTag = function (tag) {
+    const { id, content } = tag;
+    axios({
+      method: "post",
+      url: `${BASE_URL}/${id}/tag-comments/`,
+      data: {
+        content,
+      },
+      headers: {
+        Authorization: `Token ${user.token}`,
+      },
+    })
+      .then((res) => {
+        console.log("성공");
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return { createReview, createTag };
 });
