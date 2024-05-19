92f8c9cde6a10fafe9a1b1f07aaa862088734853
cadetbluee <cadetbluee@icloud.com>
2024-05-19 19:23:37 +0900

    :fire:

--- front/meova/src/components/MovieListItem.vue
+++ front/meova/src/components/MovieListItem.vue
@@ -1,32 +1,36 @@
 <template>
-    <div>
-        <div class="movie-item">
-        <RouterLink :to="{ name: 'MovieDetailView', params: { id: movie.id } }">
-            <p>{{ movie.title }}</p>
-            <img :src="'https://image.tmdb.org/t/p/original/' + movie.poster_path" alt="movie-poster" class="movie-image">
-        </RouterLink>
-        <p>{{ movie.overview }}</p>
-        </div>
+  <div>
+    <div class="movie-item">
+      <RouterLink :to="{ name: 'MovieDetailView', params: { id: movie.id } }">
+        <p>{{ movie.title }}</p>
+        <img
+          :src="'https://image.tmdb.org/t/p/original/' + movie.poster_path"
+          alt="movie-poster"
+          class="movie-image"
+        />
+      </RouterLink>
+      <p>{{ movie.overview }}</p>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { useRouter, RouterLink } from 'vue-router'
-import { useMovieStore } from '@/stores/movie'
+import { useRouter, RouterLink } from "vue-router";
+import { useMovieStore } from "@/stores/movie";
 
 const props = defineProps({
-    movie: Object
-})
+  movie: Object,
+});
 
-const store = useMovieStore()
+const store = useMovieStore();
 </script>
 
 <style scoped>
 .movie-item {
-    width: 500px;
+  width: 500px;
 }
 
 .movie-image {
-    width: 40%;
+  width: 40%;
 }
 </style>
